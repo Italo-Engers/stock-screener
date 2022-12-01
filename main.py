f445be3a214b5ce917bec64cff91885a9376e0dc
Italo <italoger@gmail.com>
2022-12-01 19:45:07 -0300

    Video 2 do exercicio código aplicado

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}   
+def home(request: Request):
+    """
+    displays the stock screener dashboard / homepage
+    """
+    return templates.TemplateResponse("home.html", {
+        "request": request
+    })
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/stock")
+def create_stock():
+    """
+    created a stock and stores in the database
+    """
+    return{
+        "code":"success",
+        "message":"stocked created"
+    }
+
+
+
